7d514dec07d200e473270bceff6db9128f5de27d
Robert Yarborough <bobby5develops@gmail.com>
2016-06-06 01:53:56 -0400

    applied angular-animate to scope of each view

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 // Declare app level module which depends on views, and components
 var myApp = angular.module('myApp', [
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngAnimate'
 ]);
 // configure our routes
 myApp.config( ['$routeProvider', '$locationProvider',  function($routeProvider, $locationProvider){
@@ -9,37 +10,41 @@ myApp.config( ['$routeProvider', '$locationProvider',  function($routeProvider, 
 
     // route for the home page
         .when('/', {
-            templateUrl : 'views/home.html',
+            templateUrl : 'views/page-home.html',
             controller  : 'mainController'
         })
 
         // route for the services page
         .when('/services', {
-            templateUrl : 'views/services.html',
+            templateUrl : 'views/page-services.html',
             controller  : 'servicesController'
         })
 
         // route for the products page
         .when('/products', {
-            templateUrl : 'views/products.html',
+            templateUrl : 'views/page-products.html',
             controller  : 'productsController'
         }).otherwise({redirectTo: '/'});
+
+    //$locationProvider.html5Mode(true);
 
 
 }]);
 
 // create the controller and inject Angular's $scope
 myApp.controller('mainController', function($scope) {
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
+    'use strict';
+    $scope.pageClass = 'page-home';
 });
 
 myApp.controller('servicesController', function($scope) {
-    $scope.message = 'Look! I am an about page.';
+    'use strict';
+    $scope.pageClass = 'page-services';
 });
 
 myApp.controller('productsController', function($scope) {
-    $scope.message = 'Contact us! JK. This is just a demo.';
+    'use strict';
+    $scope.pageClass = 'page-products';
 });
 
 
--- dist/js/app.js
+++ dist/js/app.js
@@ -9,44 +9,49 @@
 // Declare app level module which depends on views, and components
 var myApp = angular.module('myApp', [
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngAnimate'
 ]);
 // configure our routes
-myApp.config( ['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider){
-    $locationProvider.hashPrefix('!');
+myApp.config( ['$routeProvider', '$locationProvider',  function($routeProvider, $locationProvider){
     $routeProvider
 
     // route for the home page
         .when('/', {
-            templateUrl : 'views/home.html',
+            templateUrl : 'views/page-home.html',
             controller  : 'mainController'
         })
 
         // route for the services page
         .when('/services', {
-            templateUrl : 'views/services.html',
+            templateUrl : 'views/page-services.html',
             controller  : 'servicesController'
         })
 
         // route for the products page
         .when('/products', {
-            templateUrl : 'views/products.html',
+            templateUrl : 'views/page-products.html',
             controller  : 'productsController'
         }).otherwise({redirectTo: '/'});
+
+
 }]);
 
 // create the controller and inject Angular's $scope
 myApp.controller('mainController', function($scope) {
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
+    $scope.pageClass = 'page-home';
 });
 
 myApp.controller('servicesController', function($scope) {
     $scope.message = 'Look! I am an about page.';
+    $scope.pageClass = 'page-services';
 });
 
 myApp.controller('productsController', function($scope) {
     $scope.message = 'Contact us! JK. This is just a demo.';
+    $scope.pageClass = 'page-products';
 });
 
 
