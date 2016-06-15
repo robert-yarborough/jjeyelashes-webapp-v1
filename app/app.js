6348e9e8a1b2539c1bf9b2fe6664ee0dad4c1583
Robert Yarborough <bobby5develops@gmail.com>
2016-06-15 12:44:36 -0400

    updating file structure and assets

--- app/app.js
+++ app/app.js
@@ -3,8 +3,11 @@ var myApp = angular.module('myApp', [
   'ngRoute',
   'ui.bootstrap',
   'ngAnimate',
-  'duParallax'
+  'ngMaterial',
+  'duParallax',
+  'uiGmapgoogle-maps'
 ]);
+
 // configure our routes
 myApp.config( ['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider
@@ -33,10 +36,16 @@ myApp.config( ['$routeProvider', '$locationProvider', function($routeProvider, $
 }]);
 
 // create the controller and inject Angular's $scope
-myApp.controller('mainController', function($scope, parallaxHelper) {
+myApp.controller('mainController', function($scope, $interval) {
     'use strict';
     $scope.pageClass = 'page-home';
-    $scope.background = parallaxHelper.createAnimator(-0.3);
+    $scope.map = {
+        center: {
+            latitude: 40.749733,
+            longitude: -73.986924
+        },
+        zoom: 8
+    };
 });
 
 myApp.controller('servicesController', function($scope) {
@@ -108,6 +117,63 @@ myApp.controller('CarouselCtrl', function ($scope) {
         return array;
     }
 });
+myApp.controller('CarouselCtrlservices', function ($scope) {
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var currIndex = 0;
+
+    $scope.addSlide = function() {
+        var newWidth = 1280 + slides.length + 1;
+        slides.push({
+            image: 'http://lorempixel.com/' + newWidth + '/451',
+            text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 3],
+            id: currIndex++
+        });
+    };
+
+    $scope.randomize = function() {
+        var indexes = generateIndexesArray();
+        assignNewIndexesToSlides(indexes);
+    };
+
+    for (var i = 0; i < 3; i++) {
+        $scope.addSlide();
+    }
+
+    // Randomize logic below
+
+    function assignNewIndexesToSlides(indexes) {
+        for (var i = 0, l = slides.length; i < l; i++) {
+            slides[i].id = indexes.pop();
+        }
+    }
+
+    function generateIndexesArray() {
+        var indexes = [];
+        for (var i = 0; i < currIndex; ++i) {
+            indexes[i] = i;
+        }
+        return shuffle(indexes);
+    }
+
+    // http://stackoverflow.com/questions/962802#962890
+    function shuffle(array) {
+        var tmp, current, top = array.length;
+
+        if (top) {
+            while (--top) {
+                current = Math.floor(Math.random() * (top + 1));
+                tmp = array[current];
+                array[current] = array[top];
+                array[top] = tmp;
+            }
+        }
+
+        return array;
+    }
+});
 myApp.controller('CarouselCtrltestimony', function ($scope) {
     $scope.myInterval = 5000;
     $scope.noWrapSlides = false;
@@ -118,8 +184,9 @@ myApp.controller('CarouselCtrltestimony', function ($scope) {
     $scope.addSlide = function() {
         var newWidth = 1280 + slides.length + 1;
         slides.push({
-            image: '../images/testimony/slide-testimony-01.jpg/' + newWidth + '/600',
-            text: ['Nice image','Awesome photograph','That is so cool'][slides.length % 3],
+            image: '/images/img/slide-testimony-01.jpg',
+            text: ['More','Extra','Lots of','Surplus'][slides.length % 3] + ' ' +
+            ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 3],
             id: currIndex++
         });
     };
